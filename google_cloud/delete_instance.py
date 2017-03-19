e0b6f4333b2d5d6b4f28576996a79a438b6c2a37
Omar Trejo <otrenav@gmail.com>
2017-03-19 04:06:51 -0600

    Detect when inside Compute Engine without specific parameters

--- google_cloud/delete_instance.py
+++ google_cloud/delete_instance.py
@@ -10,24 +10,30 @@ from oauth2client.client import ApplicationDefaultCredentialsError
 def delete_instance(arguments):
     credentials = get_credentials()
     if credentials:
-        print(50 * '-')
-        print('DELETING GOOGLE CLOUD INSTANCE:')
-        print('- Project:  {}'.format(arguments.project))
-        print('- Zone:     {}'.format(arguments.zone))
-        print('- Instance: {}'.format(arguments.instance))
-        print(50 * '-')
-        service = discovery.build(
-            'compute',
-            'v1',
-            credentials=credentials
-        )
-        request = service.instances().delete(
-            instance=arguments.instance,
-            project=arguments.project,
-            zone=arguments.zone
-        )
-        response = request.execute()
-        pprint(response)
+        if arguments.project:
+            print(50 * '-')
+            print('DELETING GOOGLE CLOUD INSTANCE:')
+            print('- Project:  {}'.format(arguments.project))
+            print('- Zone:     {}'.format(arguments.zone))
+            print('- Instance: {}'.format(arguments.instance))
+            print(50 * '-')
+            service = discovery.build(
+                'compute',
+                'v1',
+                credentials=credentials
+            )
+            request = service.instances().delete(
+                instance=arguments.instance,
+                project=arguments.project,
+                zone=arguments.zone
+            )
+            response = request.execute()
+            pprint(response)
+        else:
+            print(50 * '-')
+            print('WARNING: INSIDE GOOGLE CLOUD INSTANCE BUT')
+            print('         NO GOOGLE CLOUD PARAMETERS SPECIFIED.')
+            print('         THE INSTANCE WILL NOT BE DELETED.')
 
 
 def get_credentials():
